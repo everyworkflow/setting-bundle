8a67e0eef2f858fef735340f62923ed1a9dc61d2
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:06:32 +0000

    Added ?ExtensionInterface

--- EveryWorkflowSettingBundle.php
+++ EveryWorkflowSettingBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\SettingBundle;
 
 use EveryWorkflow\SettingBundle\DependencyInjection\SettingExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowSettingBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new SettingExtension();
     }
